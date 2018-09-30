0c93f3b23220a7e9e6ef7883cefddfdb1ac01db5
aryouham <arhamjsiddiqui@gmail.com>
2018-09-30 18:08:14 -0500

    added test case for factors of 2

--- src/test/kotlin/com/gmail/arhamjsiddiqui/PrimeFactorsTest.kt
+++ src/test/kotlin/com/gmail/arhamjsiddiqui/PrimeFactorsTest.kt
@@ -8,9 +8,12 @@ class PrimeFactorsTest {
     @Test
     fun factors() {
         assertThat(factorsOf(1), `is`(emptyList()))
+        assertThat(factorsOf(2), `is`(listOf(2)))
     }
 
     private fun factorsOf(n: Int): List<Int> {
-        return emptyList()
+        val factors = mutableListOf<Int>()
+        if (n > 1) factors.add(2)
+        return factors
     }
 }
