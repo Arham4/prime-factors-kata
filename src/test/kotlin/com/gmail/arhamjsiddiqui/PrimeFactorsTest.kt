4b1fddabfbe93942200cd7379507773e8804f1d1
aryouham <arhamjsiddiqui@gmail.com>
2018-09-30 18:15:17 -0500

    added test case for factors of 5, 6, 7, 8

--- src/test/kotlin/com/gmail/arhamjsiddiqui/PrimeFactorsTest.kt
+++ src/test/kotlin/com/gmail/arhamjsiddiqui/PrimeFactorsTest.kt
@@ -11,18 +11,22 @@ class PrimeFactorsTest {
         assertThat(factorsOf(2), `is`(listOf(2)))
         assertThat(factorsOf(3), `is`(listOf(3)))
         assertThat(factorsOf(4), `is`(listOf(2, 2)))
+        assertThat(factorsOf(5), `is`(listOf(5)))
+        assertThat(factorsOf(6), `is`(listOf(2, 3)))
+        assertThat(factorsOf(7), `is`(listOf(7)))
+        assertThat(factorsOf(8), `is`(listOf(2, 2, 2)))
     }
 
-    private fun factorsOf(nn: Int): List<Int> {
-        var n = nn
+    private fun factorsOf(n: Int): List<Int> {
+        var remainder = n
         val factors = mutableListOf<Int>()
-        if (n > 1) {
-            if (n % 2 == 0) {
+        if (remainder > 1) {
+            while (remainder % 2 == 0) {
                 factors.add(2)
-                n /= 2
+                remainder /= 2
             }
-            if (n > 1) factors.add(n)
         }
+        if (remainder > 1) factors.add(remainder)
         return factors
     }
 }
